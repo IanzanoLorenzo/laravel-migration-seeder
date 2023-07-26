0ba68f01752303023858f911b41bbcc824ab760d
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-07-26 18:28:22 +0200

    modifiche alla pagina treni

--- resources/views/todays-trains.blade.php
+++ resources/views/todays-trains.blade.php
@@ -1,18 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="text-center">TRENI <br> I treni di oggi: {{ $data }}</h1>
+    <h1 class="text-center">TRENI <br> I treni di oggi: {{ date('d/m/Y', strtotime($data)) }}</h1>
     <div class="container mt-5">
         <div class="card">
             <ul class="list-group">
                 @foreach ($trains as $train)
                     <li class="list-group-item">
-                        {{ $train->stazione_di_partenza }}, {{ $train->orario_partenza }} - {{ $train->stazione_di_arrivo }}, {{ $train->orario_arrivo }}
+                        {{ $train->stazione_di_partenza }}, {{ date('H:i', strtotime($train->orario_partenza)) }} - {{ $train->stazione_di_arrivo }}, {{ date('H:i', strtotime($train->orario_arrivo)) }}
                         @if (!$train->in_orario)
                             @if ($train->cancellato)
                                 <span><strong>Treno cancellato</strong></span>
                             @else
-                                <span><strong>In ritardo di {{ $train->delay }}</strong></span>
+                                <span><strong>In ritardo di {{ date('i', strtotime($train->delay))  }} minuti</strong></span>
                             @endif
                         @endif
                     </li>
